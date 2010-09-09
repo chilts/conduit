441e9a49f6d272db2b273e77b99a669c0171cbad
Andrew Chilton <chilts@appsattic.com>
2010-09-09 17:52:08 +1200

    Add some more useful validation routines

--- lib/CGI/Conduit/Valid.pm
+++ lib/CGI/Conduit/Valid.pm
@@ -4,6 +4,9 @@ package CGI::Conduit::Valid;
 
 use Moose::Role;
 
+use Data::Validate::Domain qw(is_domain);
+use Email::Valid;
+
 ## ----------------------------------------------------------------------------
 
 sub valid_int {
@@ -17,6 +20,31 @@ sub valid_something {
     return unless defined $something;
     return 1 if $something =~ m{ \S }xms;
     return;
+}
+
+sub valid_domain {
+    my ($self, $domain) = @_;
+    return 1 if is_domain($domain);
+    return;
+}
+
+sub valid_ipv4 {
+    my ($self, $ip_address) = @_;
+    my @octets = split( m{\.}xms, $ip_address );
+
+    warn "o=@octets";
+
+    # check for 4 of them, between 0 and 255 inclusive
+    return 0 unless @octets == 4;
+    warn "ere";
+    foreach my $octet ( @octets ) {
+        return 0 unless $self->valid_int($octet);
+        return 0 unless ( $octet >= 0 and $octet <= 255 );
+    }
+
+    warn "was";
+
+    return 1;
 }
 
 after 'clear' => sub { };
