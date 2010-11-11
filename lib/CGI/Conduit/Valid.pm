d14d22b45ec4101ea0ae3e4e2919ec044b437d93
Andrew Chilton <chilts@appsattic.com>
2010-11-12 11:20:05 +1300

    Use the Projectus::Valid module for this stuff

--- lib/CGI/Conduit/Valid.pm
+++ lib/CGI/Conduit/Valid.pm
@@ -1,50 +1,30 @@
 ## ----------------------------------------------------------------------------
 
 package CGI::Conduit::Valid;
-
 use Moose::Role;
 
-use Data::Validate::Domain qw(is_domain);
-use Email::Valid;
+use Projectus::Valid; # export nothing, we'll be explicit here
 
 ## ----------------------------------------------------------------------------
 
-sub valid_int {
-    my ($self, $int) = @_;
-    return 1 if $int =~ m{ \A \d+ \z }xms;
-    return;
-}
-
 sub valid_something {
     my ($self, $something) = @_;
-    return unless defined $something;
-    return 1 if $something =~ m{ \S }xms;
-    return;
+    return Projectus::Valid::valid_something( $something );
+}
+
+sub valid_int {
+    my ($self, $int) = @_;
+    return Projectus::Valid::valid_int( $int );
 }
 
 sub valid_domain {
     my ($self, $domain) = @_;
-    return 1 if is_domain($domain);
-    return;
+    return Projectus::Valid::valid_domain( $domain );
 }
 
 sub valid_ipv4 {
     my ($self, $ip_address) = @_;
-    my @octets = split( m{\.}xms, $ip_address );
-
-    warn "o=@octets";
-
-    # check for 4 of them, between 0 and 255 inclusive
-    return 0 unless @octets == 4;
-    warn "ere";
-    foreach my $octet ( @octets ) {
-        return 0 unless $self->valid_int($octet);
-        return 0 unless ( $octet >= 0 and $octet <= 255 );
-    }
-
-    warn "was";
-
-    return 1;
+    return Projectus::Valid::valid_ipv4( $ip_address );
 }
 
 after 'clear' => sub { };
