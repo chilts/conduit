5499fa823fc258f9deb377d70a2329bac1e57cec
Andrew Chilton <chilts@appsattic.com>
2010-12-08 16:01:09 +1300

    Allow easy file downloads and rendering

--- lib/CGI/Conduit/File.pm
+++ lib/CGI/Conduit/File.pm
@@ -3,7 +3,9 @@
 package CGI::Conduit::File;
 
 use Moose::Role;
+use File::Basename;
 use File::Copy;
+use File::Slurp;
 
 ## ----------------------------------------------------------------------------
 
@@ -19,6 +21,35 @@ sub file_copy {
     # copy this file elsewhere
     copy($fh, $dest);
     return 1;
+}
+
+sub render_file {
+    my ($self, $filename, $path_to_file) = @_;
+
+    # if the file doesn't exist, croak
+    unless ( -f $path_to_file ) {
+        my $log = get_logger();
+        my $msg = qq{File '$path_to_file' doesn't exist};
+        $log->fatal( $msg );
+        croak $msg;
+    }
+
+    # read whole file in
+    my $data = read_file( $path_to_file, binmode => ':raw' );
+    my $size = -s $path_to_file;
+
+    # remove all newlines from the filename
+    $filename =~ s{[\n\r]+}{}gxms;
+
+    # add some headers
+    $self->hdr_add_header( q{Content-Length},      $size );
+    $self->hdr_add_header( q{Content-Disposition}, qq{attachment; filename="$filename"} );
+
+    # render the header and file with the correct content type
+    $self->render_content_with_type(
+        'application/octet-stream',
+        $data,
+    );
 }
 
 after 'clear' => sub { };
